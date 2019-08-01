7ab49c37ea26288b0f69b17d3d898b68c2e33470
Tommy F <46322567+TomFern@users.noreply.github.com>
2019-08-01 11:42:50 -0300

    emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,3 +1,11 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+;; theme
+(require 'doom-themes)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+(load-theme 'adwaita t)
+(doom-themes-visual-bell-config)
+(doom-themes-org-config)
+(doom-themes-neotree-config)
+;;(doom-themes-treemacs-config)
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -5,3 +5,6 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
+
+;(package! minimal-theme)
+(package! doom-themes)
